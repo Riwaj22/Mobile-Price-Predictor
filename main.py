43007da90c527f0449a699bd60c36584fbe41591
Riwaj Neupane <99485058+Riwaj22@users.noreply.github.com>
2023-09-04 13:37:35 +0545

    Update main.py

--- main.py
+++ main.py
@@ -65,52 +65,29 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 @cross_origin()
+@app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Get form data
-        Processor = request.form.get('Processor')
-        rating = float(request.form.get('rating'))
-        no = int(request.form.get('no'))  # Cast to int if it's an integer
-        RAM = float(request.form.get('RAM'))  # Cast to float
-        ROM = float(request.form.get('ROM'))  # Cast to float
-        f = float(request.form.get('f'))  # Cast to float
-        Battery = float(request.form.get('Battery'))  # Cast to float
-        cam = float(request.form.get('cam'))  # Cast to float
-        ty = request.form.get('ty')
-        name = request.form.get('name')
-
-        # Create a dictionary with the feature names and values
-        data_dict = {
-            'Processor': [Processor],
-            'Rating ?/5': [rating],
-            'Number of Ratings': [no],
-            'RAM': [RAM],
-            'ROM/Storage': [ROM],
-            'Front Camera': [f],
-            'Battery': [Battery],
-            'Camera Resolutions': [cam],
-            'Camera Types': [ty],
-            'Name of Phone': [name]
-        }
-
-        # Create a DataFrame
-        input_data = pd.DataFrame(data_dict)
+        # Get JSON data from the request
+        data = request.json
+        
+        # Create a DataFrame from the JSON data
+        input_data = pd.DataFrame(data)
 
         # Make the prediction
         prediction = model.predict(input_data)
 
-        return jsonify({'prediction': prediction[0]})  # Send the prediction as JSON response
+        return jsonify({'prediction': prediction.tolist()})  # Send the prediction as JSON response
     except Exception as e:
-        # Log the error
+        # Log the error (you can add more detailed logging here)
         app.logger.error(f'Prediction error: {str(e)}')
 
         # Return an error response
         return jsonify({'error': 'Prediction not available'}), 500
 
-
-
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
 
